790a31b18dc0fa6492222e2e1e7edb35577398b3
482F <482F.git.mail@482f.net>
2020-02-03 00:00:00 +0900

    直接リダイレクトができなかったので一時ファイルを作成するように

--- dump.sh
+++ dump.sh
@@ -7,14 +7,17 @@ HOSTNAME="localhost"
 PORT="58348"
 DATABASE_NAME="monster_hunter_world"
 DATABASE_DUMP_FILE="${SCRIPT_DIR}/${DATABASE_NAME}.sql"
+DATABASE_DUMP_FILE_TMP="${DATABASE_DUMP_FILE}.tmp"
 CONNECT_CONSECUTIVE_INSERT="${SCRIPT_DIR}/connect_consecutive_insert.py"
 CONNECT_CONSECUTIVE_INSERT_URL="https://raw.githubusercontent.com/482F/misc/master/connect_consecutive_insert.py"
+CHECK="${1:-}"
 
-mysqldump -h "${HOSTNAME}" -P "${PORT}" --skip-dump-date "${DATABASE_NAME}" > "${DATABASE_DUMP_FILE}"
+mysqldump -h "${HOSTNAME}" -P "${PORT}" --skip-dump-date "${DATABASE_NAME}" > "${DATABASE_DUMP_FILE_TMP}"
 
 if [ ! -e "${CONNECT_CONSECUTIVE_INSERT}" ]; then
     curl "${CONNECT_CONSECUTIVE_INSERT_URL}" > "${CONNECT_CONSECUTIVE_INSERT}"
     chmod 744 "${CONNECT_CONSECUTIVE_INSERT}"
 fi
 
-"${CONNECT_CONSECUTIVE_INSERT}" "${DATABASE_DUMP_FILE}" > "${DATABASE_DUMP_FILE}"
+"${CONNECT_CONSECUTIVE_INSERT}" "${DATABASE_DUMP_FILE_TMP}" > "${DATABASE_DUMP_FILE}"
+rm "${DATABASE_DUMP_FILE_TMP}"
