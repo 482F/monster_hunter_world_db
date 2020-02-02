6c37543e9ea243923b9232e4bc4f918b86da4d52
482F <482F.git.mail@482f.net>
2020-02-03 00:00:00 +0900

    CHECK 変数の名前を OPT に変更

--- dump.sh
+++ dump.sh
@@ -10,7 +10,7 @@ DATABASE_DUMP_FILE="${SCRIPT_DIR}/${DATABASE_NAME}.sql"
 DATABASE_DUMP_FILE_TMP="${DATABASE_DUMP_FILE}.tmp"
 CONNECT_CONSECUTIVE_INSERT="${SCRIPT_DIR}/connect_consecutive_insert.py"
 CONNECT_CONSECUTIVE_INSERT_URL="https://raw.githubusercontent.com/482F/misc/master/connect_consecutive_insert.py"
-CHECK="${1:-}"
+OPT="${1:-}"
 
 mysqldump -h "${HOSTNAME}" -P "${PORT}" --skip-dump-date "${DATABASE_NAME}" > "${DATABASE_DUMP_FILE_TMP}"
 
@@ -19,8 +19,8 @@ if [ ! -e "${CONNECT_CONSECUTIVE_INSERT}" ]; then
     chmod 744 "${CONNECT_CONSECUTIVE_INSERT}"
 fi
 
-if [ "${CHECK}" = "--check" ]; then
-    "${CONNECT_CONSECUTIVE_INSERT}" ${CHECK} "${DATABASE_DUMP_FILE_TMP}"
+if [ "${OPT}" = "--check" ]; then
+    "${CONNECT_CONSECUTIVE_INSERT}" ${OPT} "${DATABASE_DUMP_FILE_TMP}"
 else
     "${CONNECT_CONSECUTIVE_INSERT}" "${DATABASE_DUMP_FILE_TMP}" > "${DATABASE_DUMP_FILE}"
 fi
