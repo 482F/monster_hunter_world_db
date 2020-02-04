7baf9246b73cf7af8cce93d21015f04912433d0d
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2020-02-05 07:44:14 +0900

    ダンプファイルを整形するように
    https://github.com/482F/misc/blob/master/connect_consecutive_insert.py
    を使用

--- dump.sh
+++ dump.sh
@@ -3,5 +3,18 @@
 set -ue -o pipefail
 
 SCRIPT_DIR=$(cd $(dirname "${0}"); pwd)
+HOSTNAME="localhost"
+PORT="58348"
+DATABASE_NAME="monster_hunter_world"
+DATABASE_DUMP_FILE="${SCRIPT_DIR}/${DATABASE_NAME}.sql"
+CONNECT_CONSECUTIVE_INSERT="${SCRIPT_DIR}/connect_consecutive_insert.py"
+CONNECT_CONSECUTIVE_INSERT_URL="https://raw.githubusercontent.com/482F/misc/master/connect_consecutive_insert.py"
 
-mysqldump -h localhost -P 58348 --skip-dump-date monster_hunter_world > "${SCRIPT_DIR}/monster_hunter_world.sql"
+mysqldump -h "${HOSTNAME}" -P "${PORT}" --skip-dump-date "${DATABASE_NAME}" > "${DATABASE_DUMP_FILE}"
+
+if [ ! -e "${CONNECT_CONSECUTIVE_INSERT}" ]; then
+    curl "${CONNECT_CONSECUTIVE_INSERT_URL}" > "${CONNECT_CONSECUTIVE_INSERT}"
+    chmod 744 "${CONNECT_CONSECUTIVE_INSERT}"
+fi
+
+"${CONNECT_CONSECUTIVE_INSERT}" "${DATABASE_DUMP_FILE}" > "${DATABASE_DUMP_FILE}"
