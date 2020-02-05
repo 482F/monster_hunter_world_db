d5f759c9679c18d0e508fe0f044d2ee933f111ae
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2020-02-05 09:17:48 +0900

    main 関数と update 関数を作成
    --update で connect_consecutive_insert.py を再ダウンロードするように

--- dump.sh
+++ dump.sh
@@ -12,16 +12,28 @@ CONNECT_CONSECUTIVE_INSERT="${SCRIPT_DIR}/connect_consecutive_insert.py"
 CONNECT_CONSECUTIVE_INSERT_URL="https://raw.githubusercontent.com/482F/misc/master/connect_consecutive_insert.py"
 OPT="${1:-}"
 
-mysqldump -h "${HOSTNAME}" -P "${PORT}" --skip-dump-date "${DATABASE_NAME}" > "${DATABASE_DUMP_FILE_TMP}"
+function main(){
+    mysqldump -h "${HOSTNAME}" -P "${PORT}" --skip-dump-date "${DATABASE_NAME}" > "${DATABASE_DUMP_FILE_TMP}"
 
-if [ ! -e "${CONNECT_CONSECUTIVE_INSERT}" ]; then
+    if [ ! -e "${CONNECT_CONSECUTIVE_INSERT}" ]; then
+        update
+    fi
+
+    if [ "${OPT}" = "--check" ]; then
+        "${CONNECT_CONSECUTIVE_INSERT}" ${OPT} "${DATABASE_DUMP_FILE_TMP}"
+    else
+        "${CONNECT_CONSECUTIVE_INSERT}" "${DATABASE_DUMP_FILE_TMP}" > "${DATABASE_DUMP_FILE}"
+    fi
+    rm "${DATABASE_DUMP_FILE_TMP}"
+}
+
+function update(){
     curl "${CONNECT_CONSECUTIVE_INSERT_URL}" > "${CONNECT_CONSECUTIVE_INSERT}"
     chmod 744 "${CONNECT_CONSECUTIVE_INSERT}"
-fi
+}
 
-if [ "${OPT}" = "--check" ]; then
-    "${CONNECT_CONSECUTIVE_INSERT}" ${OPT} "${DATABASE_DUMP_FILE_TMP}"
+if [ "${OPT}" = "--update" ]; then
+    update
 else
-    "${CONNECT_CONSECUTIVE_INSERT}" "${DATABASE_DUMP_FILE_TMP}" > "${DATABASE_DUMP_FILE}"
+    main
 fi
-rm "${DATABASE_DUMP_FILE_TMP}"
